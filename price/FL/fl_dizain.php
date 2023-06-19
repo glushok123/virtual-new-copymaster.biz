5de1c21a4914e7710bd0e35d9d314c45472043d4
Andrey <glushok19999@gmail.com>
2023-06-19 12:50:47 +0300

    конструктор кружек + рефакторинг

--- price/FL/fl_dizain.php
+++ price/FL/fl_dizain.php
@@ -1,52 +1,56 @@
 <table id="table_7">
     <tbody>
-         <tr class="tr1">
-             <td colspan="2">Услуги дизайнера</td>
-         </tr>
-         <tr>
-             <td>Верстка макета</td>
-             <td>от <? echo $price['dis_one_vm'] ?></td>
-         </tr>
-         <tr>
-             <td>Доработка графических файлов клиента</td>
-             <td>500</td>
-         </tr>
-         <tr>
-             <td>Ретушь (реставрация) фотографий</td>
-             <td>от <? echo $price['dis_one_fr'] ?></td>
-         </tr>
-         <tr>
-             <td>Разработка дизайна логотипа</td>
-             <td>от <? echo $price['dis_one_rl'] ?></td>
-         </tr>
-         <tr>
-             <td>Фото на документы (за 1 комплект)</td>
-             <td><? echo $price['dis_fd_1'] ?></td>
-         </tr>
-         <tr>
-             <td>Комплект фото + эл.файл</td>
-             <td><? echo $price['dis_fd_2'] ?></td>
-         </tr>
-         <tr>
-             <td>Комплект фото + костюм</td>
-             <td><? echo $price['dis_fd_3'] ?></td>
-         </tr>
-         <tr>
-             <td>Комплект фото + костюм + эл.файл</td>
-             <td><? echo $price['dis_fd_4'] ?></td>
-         </tr>
-         <tr>
-             <td>Доп комплект</td>
-             <td><? echo $price['dis_fd_5'] ?></td>
-         </tr>
-         <tr>
-             <td>Распечатка готовых фото</td>
-             <td>350</td>
-         </tr>
-         <tr>
-             <td>Фото на документы с выездом на дом</td>
-             <td>3000</td>
-         </tr>
+        <tr class="tr1">
+            <td colspan="2">Услуги дизайнера</td>
+        </tr>
+        <tr>
+            <td>Верстка макета</td>
+            <td>от
+                <? echo $price['dis_one_vm'] ?></td>
+        </tr>
+        <tr>
+            <td>Доработка графических файлов клиента</td>
+            <td>500</td>
+        </tr>
+        <tr>
+            <td>Ретушь (реставрация) фотографий</td>
+            <td>от
+                <? echo $price['dis_one_fr'] ?></td>
+        </tr>
+        <tr>
+            <td>Разработка дизайна логотипа</td>
+            <td>от
+                <? echo $price['dis_one_rl'] ?></td>
+        </tr>
+        <tr>
+            <td>Фото на документы (за 1 комплект)</td>
+            <td><? echo $price['dis_fd_1'] ?></td>
+        </tr>
+        <tr>
+            <td>Комплект фото + эл.файл</td>
+            <td><? echo $price['dis_fd_2'] ?></td>
+        </tr>
+        <tr>
+            <td>Комплект фото + костюм</td>
+            <td><? echo $price['dis_fd_3'] ?></td>
+        </tr>
+        <tr>
+            <td>Комплект фото + костюм + эл.файл</td>
+            <td><? echo $price['dis_fd_4'] ?></td>
+        </tr>
+        <tr>
+            <td>Доп комплект</td>
+            <td><? echo $price['dis_fd_5'] ?></td>
+        </tr>
+        <tr>
+            <td>Распечатка готовых фото</td>
+            <td>350</td>
+        </tr>
+        <tr>
+            <td>Фото на документы с выездом на дом</td>
+            <td>3000</td>
+        </tr>
 
-     </tbody>
+    </tbody>
 </table>
+
