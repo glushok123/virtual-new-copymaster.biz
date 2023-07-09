7e93ae7ee43fbb458f95c9ba8511a0cccd44bada
Andrey <glushok19999@gmail.com>
2023-07-09 18:17:43 +0300

    открытие смены в калькуляторе

--- dashbord/calc/index.php
+++ dashbord/calc/index.php
@@ -470,7 +470,7 @@
 		<div class='butn' id='butHistoryCher'>Черновики</div>
 		<div class='butn' id='butOrders'>Заявки</div>
 		<div id='butNew' class='butn'>Новый чек</div>
-		<div class='butn' data-toggle="modal" data-target="#exampleModal" style="background-color:red; color: white">Открыть смену</div>
+		
 
 		<!--div class='container'>
 			<div class='row'>
@@ -488,31 +488,11 @@
 		<div class="switcher-btn"> <i class="bx bx-cog bx-spin"></i>
 		</div>
 		<div class="switcher-body">
-			<h5 class="mb-0 text-uppercase">Theme Customizer</h5>
+			<h5 class="mb-0 text-center text-uppercase">Действия</h5>
+			<br>
 			<hr>
-			<p class="mb-0">Gaussian Texture</p>
-			  <hr>
-			  
-			  <ul class="switcher">
-				<li id="theme1"></li>
-				<li id="theme2"></li>
-				<li id="theme3"></li>
-				<li id="theme4"></li>
-				<li id="theme5"></li>
-				<li id="theme6"></li>
-			  </ul>
-               <hr>
-			  <p class="mb-0">Gradient Background</p>
-			  <hr>
-			  
-			  <ul class="switcher">
-				<li id="theme7"></li>
-				<li id="theme8"></li>
-				<li id="theme9"></li>
-				<li id="theme10"></li>
-				<li id="theme11"></li>
-				<li id="theme12"></li>
-			  </ul>
+			<div class='butn' data-toggle="modal" data-target="#exampleModal" style="background-color:red; color: white">Закрыть предыдущую и открыть новую смену</div>
+			
 		</div>
 	</div>
 
